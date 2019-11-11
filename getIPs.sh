b33474275a183346089dfd2cad5f50bf6ef9e789
Simon <meet@simon-frey.eu>
2019-11-12 00:36:26 +0100

    Suppress curl progress bar

--- getIPs.sh
+++ getIPs.sh
@@ -1,1 +1,1 @@
-curl https://check.torproject.org/exit-addresses | sed '/ExitAddress/!d' | sed 's/ /\n/g' | sed '/\./!d' > exit_node_ips.txt
+curl -s https://check.torproject.org/exit-addresses | sed '/ExitAddress/!d' | sed 's/ /\n/g' | sed '/\./!d' > exit_node_ips.txt
